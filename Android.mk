bb0982e8e7e4518042c54c31a35a7e01fad5070f
freexperia <jerpelea@gmail.com>
2012-08-08 19:57:58 +0300

    tamsui-common: typo fix

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),tapioca_ss)
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),tamsui)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
