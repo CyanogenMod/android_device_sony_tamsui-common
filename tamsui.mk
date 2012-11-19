c361df186235b63e80f17897456d65ad1dafd33d
Giulio Cervera <giulio.cervera@gmail.com>
2012-11-19 14:34:31 +0100

    tamsui: use 7x27a Sony/Qualcomm RIL

--- tamsui.mk
+++ tamsui.mk
@@ -98,7 +98,7 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.telephony.ril_class=SonyQualcommRIL \
+    ro.telephony.ril_class=SonyQualcommRIL7x27a \
     ro.telephony.ril.v3=skipnullaid \
     rild.libpath=/system/lib/libril-qc-1.so \
     rild.libargs=-d /dev/smd0 \
