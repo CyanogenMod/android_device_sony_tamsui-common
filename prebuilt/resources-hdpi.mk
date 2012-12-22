293bf1ed88e5c3b34b16d016669b51bc55364e01
Alin Jerpelea <jerpelea@gmail.com>
2012-12-22 11:24:21 +0200

    tamsui: new charging animations
    Change-Id: Ie63318a7fc43acd1d16d77bf7fb886f65d7190e9

--- prebuilt/resources-hdpi.mk
+++ prebuilt/resources-hdpi.mk
@@ -1,9 +1,9 @@
 #Offline charging animation
 PRODUCT_COPY_FILES += \
-     device/sony/tamsui-common/prebuilt/animations/charging_animation_01_H.png:system/semc/chargemon/data/charging_animation_01.png \
-     device/sony/tamsui-common/prebuilt/animations/charging_animation_02_H.png:system/semc/chargemon/data/charging_animation_02.png \
-     device/sony/tamsui-common/prebuilt/animations/charging_animation_03_H.png:system/semc/chargemon/data/charging_animation_03.png \
-     device/sony/tamsui-common/prebuilt/animations/charging_animation_04_H.png:system/semc/chargemon/data/charging_animation_04.png \
-     device/sony/tamsui-common/prebuilt/animations/charging_animation_05_H.png:system/semc/chargemon/data/charging_animation_05.png \
-     device/sony/tamsui-common/prebuilt/animations/charging_animation_06_H.png:system/semc/chargemon/data/charging_animation_06.png \
-     device/sony/tamsui-common/prebuilt/animations/charging_animation_07_H.png:system/semc/chargemon/data/charging_animation_07.png
+     device/sony/tamsui-common/prebuilt/animations/ca01_854x480.rle:system/etc/chgani/ca01.rle \
+     device/sony/tamsui-common/prebuilt/animations/ca02_854x480.rle:system/etc/chgani/ca02.rle \
+     device/sony/tamsui-common/prebuilt/animations/ca03_854x480.rle:system/etc/chgani/ca03.rle \
+     device/sony/tamsui-common/prebuilt/animations/ca04_854x480.rle:system/etc/chgani/ca04.rle \
+     device/sony/tamsui-common/prebuilt/animations/ca05_854x480.rle:system/etc/chgani/ca05.rle \
+     device/sony/tamsui-common/prebuilt/animations/ca06_854x480.rle:system/etc/chgani/ca06.rle \
+     device/sony/tamsui-common/prebuilt/animations/ca07_854x480.rle:system/etc/chgani/ca07.rle
