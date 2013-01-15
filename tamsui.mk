c0364ed1022614cfaa85ee1a74c665e9dfd9f6be
Alin Jerpelea <jerpelea@gmail.com>
2013-01-15 20:14:28 +0200

    tamsui: FM Radio support
    Change-Id: I845ad66ac2f6fcd432748a166f276289867fed2f

--- tamsui.mk
+++ tamsui.mk
@@ -16,6 +16,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/com.stericsson.hardware.fm.receiver.xml:system/etc/permissions/com.stericsson.hardware.fm.receiver.xml \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
 
 # EGL config
@@ -71,6 +72,7 @@ PRODUCT_PACKAGES += \
 
 # Misc
 PRODUCT_PACKAGES += \
+    FmRadioReceiver \
     com.android.future.usb.accessory
 
 # Live Wallpapers
