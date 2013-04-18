2102624f0325b8c159cf6abd42b0285ba0c4e8c3
Alin Jerpelea <jerpelea@gmail.com>
2013-04-18 18:33:27 +0300

    tamsui: disable build on legacy devices
    Change-Id: Id9b289fb2f1bffbeb6e6280081e7417e7bcb3a4f

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
+ifneq ($(TARGET_USES_LEGACY_BOARD),true)
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),tamsui)
     include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
 endif
